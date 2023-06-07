769b4a2f18ef12ecde6539724f06e3c253f11a4f
Uwe Schmidt <uschmidt83@users.noreply.github.com>
2023-06-07 15:22:53 +0200

    Bugfix

--- unet_2d/model.py
+++ unet_2d/model.py
@@ -285,7 +285,7 @@ class UNet(CARE):
             not IS_TF_1 and not any(isinstance(cb,CARETensorBoardImage) for cb in self.callbacks)):
             self.callbacks.append(CARETensorBoardImage(model=self.keras_model, data=(xv, yv),
                                                        log_dir=str(self.logdir/'logs'/'images'),
-                                                       n_images=3, prob_out=self.config.probabilistic),
+                                                       n_images=3, prob_out=self.config.probabilistic,
                                                        **({} if care_tb_kwargs is None else care_tb_kwargs))
 
         fit = self.keras_model.fit_generator if IS_TF_1 else self.keras_model.fit
